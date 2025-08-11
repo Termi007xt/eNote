b69a01301311723ff94e64ccbf4b014f3ca39104
Maulik Sharma <sharmamaulik95@gmail.com>
2025-08-11 14:38:05 +0530

    6.0 Added JWT (auth Token)

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const User = require("../models/User");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "NoHecking@MeBoi";
 
 //Create a user using : POST "/api/auth/createuser". No login required
 router.post(
@@ -30,13 +34,26 @@ router.post(
           .status(400)
           .json({ error: "Sorry, a user with this email aready exists" });
       }
+
+      const salt = await bcrypt.genSaltSync(10);
+      const securePass = await bcrypt.hashSync(req.body.password, salt);
+
+      //Create new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: securePass,
       });
 
-      res.json({ user });
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+
+      res.json({ authToken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error occured");
