6a513ddaf1f4b772ac4c1c1d392d22f8d940cbab
Maulik Sharma <sharmamaulik95@gmail.com>
2025-08-14 21:02:23 +0530

    7.0 Added FetchNotes API req

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fetchUser = require("../middleware/fetchUser");
 
 const JWT_SECRET = "NoHecking@MeBoi";
 
-//Create a user using : POST "/api/auth/createuser". No login required
+//ROUTE 1: Create a user using : POST "/api/auth/createuser". No login required
 router.post(
   "/createuser",
   [
@@ -56,9 +57,66 @@ router.post(
       res.json({ authToken });
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured");
+      res.status(500).send("Internal server error occured");
     }
   }
 );
+
+//ROUTE 2: Authenticate a user using : POST "/api/auth/login".
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email.").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //If there are errors, return Bad request and the errors, dont crash.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error occured");
+    }
+  }
+);
+
+//ROUTE 3: Get loggin in user details : POST "/api/auth/getuser". Login required
+router.post("/getuser", [], fetchUser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error occured");
+  }
+});
 
 module.exports = router;
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const fetchUser = require("../middleware/fetchUser");
+const Notes = require("../models/Notes");
 
-router.get("/", (req, res) => {
-  res.json([]);
+//ROUTE 1: Get all notes : GET "/api/auth/getuser". Login required
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  const notes = await Notes.find({ user: req.user.id });
+  res.json(notes);
 });
 
 module.exports = router;
