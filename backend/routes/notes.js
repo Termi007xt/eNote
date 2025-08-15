734c78a5967605fc2c82c3b2606d40d3b8cf574d
Maulik Sharma <sharmamaulik95@gmail.com>
2025-08-15 20:39:58 +0530

    9.0 Can update notes now

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -4,13 +4,13 @@ const fetchUser = require("../middleware/fetchUser");
 const Notes = require("../models/Notes");
 const { body, validationResult } = require("express-validator");
 
-//ROUTE 1: Get all notes : GET "/api/auth/getuser". Login required
+//ROUTE 1: Get all notes : GET "/api/notes/getuser". Login required
 router.get("/fetchallnotes", fetchUser, async (req, res) => {
   const notes = await Notes.find({ user: req.user.id });
   res.json(notes);
 });
 
-//ROUTE 2: Add notes : POST "/api/auth/addnote". Login required
+//ROUTE 2: Add notes : POST "/api/notes/addnote". Login required
 router.post(
   "/addnote",
   fetchUser,
@@ -42,6 +42,66 @@ router.post(
       });
       const saveNote = await note.save();
       res.json(saveNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error occured");
+    }
+  }
+);
+
+//ROUTE 3: Update an existing note : POST "/api/notes/updatenote". Login required
+router.put(
+  "/updatenote/:id",
+  fetchUser,
+  [
+    body("title", "Enter a valid title (atleast 2 characters).").isLength({
+      min: 2,
+    }),
+    body(
+      "description",
+      "Enter a valid description (atleast 3 characters)."
+    ).isLength({
+      min: 3,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+
+      //If there are errors, return Bad request and the errors, dont crash.
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const newNote = {};
+
+      if (title) {
+        newNote.title = title;
+      }
+      if (description) {
+        newNote.description = description;
+      }
+      if (tag) {
+        newNote.tag = tag;
+      }
+
+      //Find the note to be updated and update it
+      let note = await Notes.findById(req.params.id);
+      if (!note) {
+        return res.status(404).send("Not found");
+      }
+
+      if (note.user.toString() !== req.user.id) {
+        return res.status(401).send("Unauthorized user");
+      }
+
+      note = await Notes.findByIdAndUpdate(
+        req.params.id,
+        { $set: newNote },
+        { new: true }
+      );
+      res.json(note);
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Internal server error occured");
