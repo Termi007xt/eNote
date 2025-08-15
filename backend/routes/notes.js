6dcebf1892651cee8f0a8ad1b78066577feb72ea
Maulik Sharma <sharmamaulik95@gmail.com>
2025-08-15 21:07:08 +0530

    10.0 Backend made

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -49,7 +49,7 @@ router.post(
   }
 );
 
-//ROUTE 3: Update an existing note : POST "/api/notes/updatenote". Login required
+//ROUTE 3: Update an existing note : PUT "/api/notes/updatenote". Login required
 router.put(
   "/updatenote/:id",
   fetchUser,
@@ -108,5 +108,33 @@ router.put(
     }
   }
 );
+
+//ROUTE 4: Delete an existing note : DELETE "/api/notes/deletenote". Login required
+router.delete("/deletenote/:id", fetchUser, async (req, res) => {
+  try {
+    //If there are errors, return Bad request and the errors, dont crash.
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //Find the note to be delete and delete it
+    let note = await Notes.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not found");
+    }
+
+    //Allow user to delete if owner.
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Unauthorized user");
+    }
+
+    note = await Notes.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note has been deleted", note: note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error occured");
+  }
+});
 
 module.exports = router;
