86bc8c85709dbfa04c10288baef3f7b783da5195
Maulik Sharma <sharmamaulik95@gmail.com>
2025-08-16 18:35:47 +0530

    12.0 Example usage of state (context api)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import "./App.css";
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import NoteState from "./context/notes/NoteState";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is eNote</h1>
-    </div>
+    <>
+      <NoteState>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+          </Routes>
+        </Router>
+      </NoteState>
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+import React, { useContext, useEffect } from "react";
+import NoteContext from "../context/notes/NoteContext";
+
+const About = () => {
+  const a = useContext(NoteContext);
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <div>
+      This is about {a.state.name} and he belongs to CSE {a.state.section}
+    </div>
+  );
+};
+
+export default About;
